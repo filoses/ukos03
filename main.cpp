1ef95f07c7911c160d1ee337112abd675f2bb654
Filip Labuda <s24808@szuflandia.pjwstk.edu.pl>
2021-11-06 14:17:06 +0100

    Pierwszy kod kalkulatora

---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+use namespace std;
+
+int a,b,c;
+
+int main() {
+    cout << "Podaj 3 liczby" << endl;
+    cin >> a, b, c;
+    cout << a+b+c/3 << endl;
+    return 0;
+}
+
